dcceca1c825b45e1a249377dee0dd9fa219219f3
carolhuang1114 <carolhuang1114@gmail.com>
2024-04-29 19:02:20 +0800

    add v1.2 tag

--- helloworld.js
+++ helloworld.js
@@ -1,6 +1,4 @@
 console.log('hello world');
-<<<<<<< HEAD
 console.log('welcome to git study, changes from other person');
-=======
 console.log('welcome to merge conflict');
->>>>>>> merge-conflict
+console.log('add v1.2 tag');
